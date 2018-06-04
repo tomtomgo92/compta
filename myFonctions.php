5649ff7dac5c27793fc1e040e1dc7c5aade05a7b
Thomas Millet <tm.lepoleswebforce3@gmail.com>
2018-06-04 12:13:41 +0200

    init

---
+++ myFonctions.php
@@ -0,0 +1,21 @@
+<?php
+
+class myMathFunc 
+{
+    function sum($x, $y)
+    {
+    $z = $x + $y;
+
+    return $z;
+    }
+
+    function min($x, $y)
+    {
+        $z = $x - $y;
+
+        return $z;
+    }
+
+}
+
+?>
