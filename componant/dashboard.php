3dcde6c291ad1b4e587f874395003f20d67bb3b0
Thomas Millet <tm.lepoleswebforce3@gmail.com>
2018-06-05 10:35:15 +0200

    menage

--- componant/dashboard.php
+++ componant/dashboard.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
+    <link href="./Assets/css/conf.min.css" rel="stylesheet">
   </head>
 
   <body>
